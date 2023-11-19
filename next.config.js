00771abee8d7497fce08a928da11cbd8238438be
iradbouzidi <b.irad1994@gmail.com>
2023-11-19 16:11:18 +0100

    set host as images server

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "127.0.0.1",
-      "next-real-estate-app-client.vercel.app",
-      "scontent.ftun8-1.fna.fbcdn.net",
-    ],
+    domains: ["127.0.0.1"],
   },
 };
